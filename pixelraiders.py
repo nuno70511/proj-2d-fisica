7a5bd0be61fbb8dfbc1ecba3d786ce5dbe262963
Nuno <9170511@esmad.ipp.pt>
2019-11-24 20:46:11 +0000

    introduzir clip e reload no tanque

--- pixelraiders.py
+++ pixelraiders.py
@@ -1,6 +1,6 @@
 import pygame, time, sys, math, random
 from pygame import K_ESCAPE, K_LEFT, K_RIGHT, K_SPACE, K_RETURN
-from bullet import Bullet
+from bullet import SmallBullet, LargeBullet, MassiveBullet, FastBullet
 from target import WeakTarget, ToughTarget, StrongTarget, KnockbackTarget
 from tank import Tank
 
@@ -23,15 +23,13 @@ MSG_GAMEOVER = get_font(48).render("GAME OVER!", True, RED, BLACK)
 MSG_NEWGAME = get_font(12).render("PRESS [ENTER] TO PLAY A NEW GAME", True, WHITE, BLACK)
 MSG_QUIT = get_font(8).render("PRESS [ESC] AT ANY TIME TO QUIT THE GAME", True, WHITE, BLACK)
 
-vx = 10
-vy = 10
 dt = 3
 bullets = []                        #   guarda as instâncias das balas
 targets = []                        #   guarda as instâncias dos alvos
 targets_move_right = True           #   condição de os alvos estarem a mover-se para a direita
 targets_move_down = False           #   condição de os alvos moverem-se para baixo
 
-tank = Tank(WIN_WIDTH / 2 - 20, WIN_HEIGHT - 20, 40, 20)     # instancializar o tanque
+tank = Tank(WIN_WIDTH / 2 - 20, WIN_HEIGHT - 20, 40, 20, 10, 4, 4, 2)     # instancializar o tanque
 
 # instancializar 10 alvos
 targets.extend([
@@ -43,11 +41,12 @@ while True:
     win.fill(BLACK)
 
     for event in pygame.event.get():
-        if event.type == pygame.KEYUP and dt != 0:
+        if event.type == pygame.KEYUP and dt != 0 and tank.clip > 0:
             if event.key == pygame.K_SPACE: # ao premir espaço, instanciar uma bala a partir da posição do tanque
-                x = tank.x + tank.width / 2 - 3 # centro do canhão
+                x = tank.x + tank.width / 2 - 8 / 2 # centro do canhão
                 y = tank.y - 10
-                bullets.append(Bullet(x, y))
+                bullets.append(SmallBullet(x, y))
+                tank.clip -= 1
 
     key_pressed = pygame.key.get_pressed()
     if key_pressed[K_LEFT] and dt != 0: # mover o tanque para a esquerda
@@ -64,12 +63,14 @@ while True:
         targets = []
         targets_move_right = True
         tank.x = WIN_WIDTH / 2 - 20
+        tank.clip = 4
+        tank.reload_timer = 0
         targets.extend([
             StrongTarget(20, 40), ToughTarget(70, 40), KnockbackTarget(120, 40), ToughTarget(170, 40), StrongTarget(220, 40),
             WeakTarget(20, 90),   WeakTarget(70, 90),  WeakTarget(120, 90),      WeakTarget(170, 90),  WeakTarget(220, 90)
         ])
         dt = 3
-    
+
 
     # desenhar o tanque
     pygame.draw.rect(win, WHITE, (tank.x, tank.y, tank.width, tank.height), 0)
@@ -77,12 +78,12 @@ while True:
     # comportamento das balas
     for bullet in bullets:
 
-        pygame.draw.circle(win, BLUE, ((int)(bullet.x), (int)(bullet.y)), 6, 0) # desenhar as balas
+        pygame.draw.circle(win, BLUE, ((int)(bullet.x), (int)(bullet.y)), bullet.radius, 0) # desenhar as balas
 
         if not targets : break  # se não houver alvos, o jogo está parado e as balas ficarão estáticas
                                 # logo, não interessa estudar o seu comportamento e o ciclo é quebrado
 
-        bullet.y -= 10 * dt * 0.1 # mover as balas
+        bullet.y -= bullet.vy * dt * 0.1 # mover as balas
 
         for target in targets:                                              # para cada alvo,
             if (bullet.y >= target.y - 10 and bullet.y <= target.y + 10     # se uma bala está à mesma altitude dos alvos
@@ -102,8 +103,8 @@ while True:
         pygame.draw.circle(win, target.color, ((int)(target.x), (int)(target.y)), 10, 0)
 
         # mover os alvos
-        if targets_move_right : target.x += vx * dt * 0.1
-        else : target.x -= vx * dt * 0.1
+        if targets_move_right : target.x += tank.vx * dt * 0.1
+        else : target.x -= tank.vx * dt * 0.1
 
         # se houver colisão entre um alvo e o tanque, apresentar mensagem de jogo perdido
         if target.y >= tank.y and target.x >= tank.x and target.x <= tank.x + tank.width:
@@ -146,8 +147,15 @@ while True:
             [ WIN_WIDTH / 2 - MSG_NEWGAME.get_width() / 2, WIN_HEIGHT / 2 - MSG_NEWGAME.get_height() / 2 + 50 ]
         )
 
+    #   apresentar número de balas no clip
+    MSG_BULLETS = get_font(10).render("BULLETS: %s" %tank.clip, True, WHITE, BLACK)
+    win.blit(MSG_BULLETS, [WIN_WIDTH - 124, WIN_HEIGHT - 14])
+
     #   instrução de fechar o pgm
     win.blit(MSG_QUIT, [4, 4])
+
+    #   se o jogo estiver em andamento, incrementar o timer de reload do tanque
+    if dt != 0 : tank.inc_timer(0.015)
 
     time.sleep(0.015)
     pygame.display.flip()
--- tank.py
+++ tank.py
@@ -1,6 +1,17 @@
 class Tank:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, vx, clip, clip_size, reload_duration):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.vx = vx
+        self.clip = clip
+        self.clip_size = clip_size
+        self.reload_duration = reload_duration
+        self.reload_timer = 0
+
+    def inc_timer(self, seconds):
+        self.reload_timer += seconds
+        if self.reload_timer > self.reload_duration:
+            self.reload_timer = 0
+            if self.clip < self.clip_size : self.clip += 1
