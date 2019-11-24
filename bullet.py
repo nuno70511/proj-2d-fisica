a3ac02223a5f037f936d9a655ff25451006bc7dc
Nuno <9170511@esmad.ipp.pt>
2019-11-24 20:45:28 +0000

    introdução de múltiplos tipos de balas

--- bullet.py
+++ bullet.py
@@ -2,3 +2,36 @@ class Bullet:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class SmallBullet(Bullet):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.id = 0
+        self.vy = 10
+        self.radius = 8
+        self.damage = 1
+        self.color = (192, 192, 192)
+
+class LargeBullet(SmallBullet):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.id = 1
+        self.vy = 6.66
+        self.radius = 12
+        self.damage = 2
+
+class MassiveBullet(SmallBullet):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.id = 2
+        self.vy = 3.33
+        self.radius = 24
+        self.damage = 4
+
+class FastBullet(SmallBullet):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.id = 3
+        self.vy = 15
+        self.radius = 8
+        self.damage = 1
